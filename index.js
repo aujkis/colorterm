aaf4d8e297f15c029d56decb055f79ea88ac4664
Austris Landmanis <austris@chevron.lv>
2015-07-12 15:24:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -74,38 +74,279 @@ internals.Colorterm.prototype.response = function (object) {
 
 
 internals.Colorterm.prototype._isSerializable = function (input, primitives) {
+  if (typeof input === 'boolean' ||
+      typeof input === 'number' || input === null ||
+  input instanceof Date) {
+    return true;
+  }
+  if (typeof input === 'string' && input.indexOf('\n') === -1) {
+    return true;
+  }
 
+  if (true && primitives) {
+    if (Array.isArray(input) && this._isSerializable(input[0], true)) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
-internals.Colorterm.prototype._parseObject = function (level, object) {
-  // console.log(this._formatLevel(level, true)+' This is object log');
+// internals.Colorterm.prototype._indentRows = function (object, spaces) {
+//   var rows = object.split('\n');
+//
+//   rows = rows.map(function (line) {
+//     return new Array(spaces + 1).join(' ') + line;
+//   });
+//
+//   return rows.join('\n');
+// }
+
+internals.Colorterm.prototype._parseObject = function (level, object, indentation) {
+
   var self = this;
   var lines = [];
 
-  lines.push(this._formatLevel(level, true) + Colors.magenta.dim(' [object placeholder]'));
+  indentation = indentation || 0;
 
-  // lines.push(this._formatLevel(level, true)+' This is object line')
-  // lines.push(this._formatLevel(level, true)+' This is object line')
-  // lines.push(this._formatLevel(level, true)+' This is object line')
+  if (this._isSerializable(object)) {
+    lines.push(new Array(indentation + 1).join(' ') + object);
+  }
+  // else if (typeof data === 'string') {
+  //   lines.push(new Array(indentation + 1).join(' ') + '"""');
+  //   lines.push(this._indentRows(object, indentation));
+  //   lines.push(new Array(indentation + 1).join(' ') + '"""');
+  // }
+  else if (Array.isArray(object)) {
+    // If the array is empty, render the `emptyArrayMsg`
+    if (object.length === 0) {
+      lines.push(self._formatLevel(level, true) + ' ' + new Array(indentation + 1).join(' ') + 'empty array');
+    } else {
+      object.forEach(function (element) {
+        // Prepend the dash at the begining of each array's element line
+        var line = self._colorLevel(level, '- ', true);
 
-  return lines.join('\n');
+        line = self._formatLevel(level, true) + ' ' + new Array(indentation + 1).join(' ') + line;
+
+        // If the element of the array is a string, bool, number, or null
+        // render it in the same line
+        if (self._isSerializable(element)) {
+          line += self._parseObject(level, element);
+          lines.push(line);
+
+        // If the element is an array or object, render it in next line
+        } else {
+          lines.push(line);
+          lines.push(self._parseObject(level, element, indentation));
+        }
+      });
+    }
+  }
+  else if (typeof object === 'object') {
+    // Get the size of the longest index to align all the values
+    var key;
+    var error = object instanceof Error;
+
+    Object.getOwnPropertyNames(object).forEach(function (index) {
+      // Prepend the index at the beginning of the line
+      key = self._colorLevel(level, (index + ': '), true);
+
+      key = self._formatLevel(level, true) + ' ' + new Array(indentation + 1).join(' ') + key;
+
+      // Skip `undefined`, it's not a valid JSON value.
+      if (object[index] === undefined) {
+        return;
+      }
+
+      // If the value is serializable, render it in the same line
+      if (self._isSerializable(object[index]) && (!error || index !== 'stack')) {
+        key += self._parseObject(level, object[index]);
+        lines.push(key);
+
+        // If the index is an array or object, render it in next line
+      } else {
+        lines.push(key);
+        lines.push(
+          self._parseObject(
+            level,
+            error && index === 'stack' ? object[index].split('\n') : object[index],
+            indentation + 2
+          )
+        );
+      }
+    });
+  }
+
+  if (lines.length > 0) {
+    return lines.join('\n');
+  }
+
+  // lines.push(this._formatLevel(level, true) + Colors.magenta.dim(' [object placeholder]'));
+
   // if (lines.length > 0) {
   //   this._stdout(lines.join('\n'));
   // }
+  // Default values
+
+  // indentation = indentation || 0;
+  // options = options || {};
+  // options.emptyArrayMsg = options.emptyArrayMsg || '(empty array)';
+  // options.keysColor = options.keysColor || 'green';
+  // options.dashColor = options.dashColor || 'green';
+  // options.numberColor = options.numberColor || 'blue';
+  // options.defaultIndentation = options.defaultIndentation || 2;
+  // options.noColor = !!options.noColor;
+  //
+  // options.stringColor = options.stringColor || null;
+  //
+  // var output = [];
+  //
+  // // Helper function to detect if an object can be directly serializable
+  // var isSerializable = function(input, onlyPrimitives) {
+  //   if (typeof input === 'boolean' ||
+  //       typeof input === 'number' || input === null ||
+  //   input instanceof Date) {
+  //     return true;
+  //   }
+  //   if (typeof input === 'string' && input.indexOf('\n') === -1) {
+  //     return true;
+  //   }
+  //
+  //   if (options.inlineArrays && !onlyPrimitives) {
+  //     if (Array.isArray(input) && isSerializable(input[0], true)) {
+  //       return true;
+  //     }
+  //   }
+  //
+  //   return false;
+  // };
+  //
+  // var indentLines = function(string, spaces){
+  //   var lines = string.split('\n');
+  //   lines = lines.map(function(line){
+  //     return Utils.indent(spaces) + line;
+  //   });
+  //   return lines.join('\n');
+  // };
+  //
+  // var addColorToData = function(input) {
+  //   if (options.noColor) {
+  //     return input;
+  //   }
+  //
+  //   if (typeof input === 'string') {
+  //     // Print strings in regular terminal color
+  //     return options.stringColor ? input[options.stringColor] : input;
+  //   }
+  //
+  //   var sInput = input + '';
+  //
+  //   if (input === true) {
+  //     return sInput.green;
+  //   }
+  //   if (input === false) {
+  //     return sInput.red;
+  //   }
+  //   if (input === null) {
+  //     return sInput.grey;
+  //   }
+  //   if (typeof input === 'number') {
+  //     return sInput[options.numberColor];
+  //   }
+  //   if (Array.isArray(input)) {
+  //     return input.join(', ');
+  //   }
+  //
+  //   return sInput;
+  // };
+  //
+  // // Render a string exactly equal
+  // if (isSerializable(data)) {
+  //   output.push(Utils.indent(indentation) + addColorToData(data));
+  // }
+  // else if (typeof data === 'string') {
+  //   //unserializable string means it's multiline
+  //   output.push(Utils.indent(indentation) + '"""');
+  //   output.push(indentLines(data, indentation + options.defaultIndentation));
+  //   output.push(Utils.indent(indentation) + '"""');
+  // }
+  // else if (Array.isArray(data)) {
+  //   // If the array is empty, render the `emptyArrayMsg`
+  //   if (data.length === 0) {
+  //     output.push(Utils.indent(indentation) + options.emptyArrayMsg);
+  //   } else {
+  //     data.forEach(function(element) {
+  //       // Prepend the dash at the begining of each array's element line
+  //       var line = ('- ');
+  //       if (!options.noColor) {
+  //         line = line[options.dashColor];
+  //       }
+  //       line = Utils.indent(indentation) + line;
+  //
+  //       // If the element of the array is a string, bool, number, or null
+  //       // render it in the same line
+  //       if (isSerializable(element)) {
+  //         line += self._parseObject(element, options);
+  //         output.push(line);
+  //
+  //       // If the element is an array or object, render it in next line
+  //       } else {
+  //         output.push(line);
+  //         output.push(self._parseObject(
+  //           element, options, indentation + options.defaultIndentation
+  //         ));
+  //       }
+  //     });
+  //   }
+  // }
+  // else if (typeof data === 'object') {
+  //   // Get the size of the longest index to align all the values
+  //   var maxIndexLength = Utils.getMaxIndexLength(data);
+  //   var key;
+  //   var isError = data instanceof Error;
+  //
+  //   Object.getOwnPropertyNames(data).forEach(function(i) {
+  //     // Prepend the index at the beginning of the line
+  //     key = (i + ': ');
+  //     if (!options.noColor) {
+  //       key = key[options.keysColor];
+  //     }
+  //     key = Utils.indent(indentation) + key;
+  //
+  //     // Skip `undefined`, it's not a valid JSON value.
+  //     if (data[i] === undefined) {
+  //       return;
+  //     }
+  //
+  //     // If the value is serializable, render it in the same line
+  //     if (isSerializable(data[i]) && (!isError || i !== 'stack')) {
+  //       key += self._parseObject(data[i], options, maxIndexLength - i.length);
+  //       output.push(key);
+  //
+  //       // If the index is an array or object, render it in next line
+  //     } else {
+  //       output.push(key);
+  //       output.push(
+  //         self._parseObject(
+  //           isError && i === 'stack' ? data[i].split('\n') : data[i],
+  //           options,
+  //           indentation + options.defaultIndentation
+  //         )
+  //       );
+  //     }
+  //   });
+  // }
+  //
+  // // Return all the lines as a string
+  // return output.join('\n');
 }
 
-//
-//
-//
-internals.Colorterm.prototype._formatLevel = function (event, hidden) {
-  if (hidden == true) {
-    var output = string('|').padLeft(10, ' ');
-  }
-  else {
-    var output = string(event + ' |').padLeft(10, ' ');
+internals.Colorterm.prototype._colorLevel = function(level, output, dim) {
+  if (dim == true) {
+    output = Colors.dim(output);
   }
 
-  switch(event) {
+  switch(level) {
     case 'log':
       return Colors.bold.white.dim(output);
       break;
@@ -131,6 +372,19 @@ internals.Colorterm.prototype._formatLevel = function (event, hidden) {
       return Colors.white(output);
   }
 }
+//
+//
+//
+internals.Colorterm.prototype._formatLevel = function (level, hidden) {
+  if (hidden == true) {
+    var output = this._colorLevel(level, string('|').padLeft(10, ' '));
+  }
+  else {
+    var output = this._colorLevel(level, string(level + ' |').padLeft(10, ' '));
+  }
+
+  return output;
+}
 
 internals.Colorterm.prototype._formatTimestamp = function (timestamp) {
   var moment = Moment.utc(timestamp);
@@ -155,7 +409,10 @@ internals.Colorterm.prototype._writeEvent = function(level, object) {
     line['data'] = Colors.white('Write object to string parser for console.' + level + '(object)');
 
     this._writeString(line);
-    this._stdout(this._parseObject(level, object));
+
+    if (this._parseObject(level, object)) {
+      this._stdout(this._parseObject(level, object));
+    }
   }
   else {
     line['event'] = 'error';
